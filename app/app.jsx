21591da1e9590de62af477e2a199bdb83183c9f6
Julia Himmel <himmel.julia@gmail.com>
2017-07-03 18:41:35 -0400

    partial upgrade of router

--- app/app.jsx
+++ app/app.jsx
@@ -1,12 +1,12 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var {Route, Router, IndexRoute, hashHistory, Link} = require('react-router');
+var {Route, Router, IndexRoute, hashHistory, Link, browserHistory} = require('react-router');
 var Main = require('Main');
 var AllProjects = require('AllProjects');
 var OneProject = require('OneProject');
 
 ReactDOM.render(
-  <Router hash={hashHistory}>
+  <Router history={hashHistory}>
     <Route path='/' component={Main} />
     <Route path='/projects' component={AllProjects}/>
     <Route path='/projects/:id' component={OneProject}/>
