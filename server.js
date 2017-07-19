ac7aee82e77a89fad74b5c2a5ceae85aa21174c9
Julia Himmel <himmel.julia@gmail.com>
2017-07-19 17:20:16 -0400

    update server.js

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var express = require('express');
 
+// Create our app
 var app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
-app.listen(3000, function(){
-  console.log('Server is up on Port 3000')
-})
+app.listen(PORT, function () {
+  console.log('Express server is up on port ' + PORT);
+});
