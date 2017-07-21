e47a195f6c0ac7ee90b5bd75e90cf997712fc5f2
Julia Himmel <himmel.julia@gmail.com>
2017-07-21 16:34:27 -0400

    add last projects

--- app/data/projects.js
+++ app/data/projects.js
@@ -19,9 +19,9 @@ export default {
             shortDescription: 'CareerFoundry sells online, mentored courses, and the student dashboard is crucial for motivating students to continue a course, supporting the relationship with the mentor, and letting students get where they want to go within a course.',
             longDescription: 'CareerFoundry sells online, mentored courses, and the student dashboard is crucial for motivating students to continue a course, supporting the relationship with the mentor, and letting students get where they want to go within a course. With the guidance of a senior UX designer, I created an information architecture and sketched wireframes, and, after several rounds of feedback from the cofounders, created prototypes and conducted user testing sessions with CareerFoundry students  in our office. Then, I worked with another developer to build the dashboard.',
             images: ["https://s3.amazonaws.com/julia-himmel-personal-site/Projects/CareerFoundry+Dashboard/cf-dash-sketch-v1.JPG", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/CareerFoundry+Dashboard/CF-dash-june-15-v2.jpg", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/CareerFoundry+Dashboard/CF-dash-june-15-v4+(1).jpg", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/CareerFoundry+Dashboard/CF-dash-june-15-v6_June+25-3.jpg", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/CareerFoundry+Dashboard/Screen+Shot+2015-11-05+at+14.07.10.png", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/CareerFoundry+Dashboard/Screen+Shot+2015-12-09+at+17.39.52.png", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/CareerFoundry+Dashboard/Screen+Shot+2015-12-09+at+17.44.24.png", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/CareerFoundry+Dashboard/Screen+Shot+2015-12-09+at+17.44.50.png", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/CareerFoundry+Dashboard/Screen+Shot+2017-01-02+at+11.57.08.png", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/CareerFoundry+Dashboard/Screen+Shot+2017-04-28+at+1.40.48+PM.png", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/CareerFoundry+Dashboard/Screen+Shot+2017-04-28+at+1.41.02+PM.png"]
-          }
+          },
           {
-            id: 3,
+            id: 2,
             tags: ['code', 'design'],
             title: 'CareerFoundry Course Prep',
             subtitle: 'An onboarding site for CareerFoundry students',
@@ -30,7 +30,7 @@ export default {
             images: ["https://s3.amazonaws.com/julia-himmel-personal-site/Projects/CareerFoundry+Onboarding+Site/CF-dash-onboarding-august-13-v2-1.jpg", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/CareerFoundry+Onboarding+Site/CF-dash-onboarding-august-13-v2-2.jpg", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/CareerFoundry+Onboarding+Site/CF-dash-onboarding-august-18-v1-1.jpg", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/CareerFoundry+Onboarding+Site/CF-dash-onboarding-august-18-v1-2.jpg", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/CareerFoundry+Onboarding+Site/Screen+Shot+2017-07-20+at+5.17.00+PM.png", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/CareerFoundry+Onboarding+Site/Screen+Shot+2017-07-20+at+5.18.25+PM.png", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/CareerFoundry+Onboarding+Site/Screen+Shot+2017-07-20+at+5.18.53+PM.png", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/CareerFoundry+Onboarding+Site/Screen+Shot+2017-07-20+at+5.19.16+PM.png"]
           },
           {
-            id: 4,
+            id: 3,
             tags: ['design'],
             title: 'Cocktail Cards',
             subtitle: 'Personal project: illustrated postcards with recipes for classic cocktails',
@@ -39,16 +39,16 @@ export default {
             images: ["https://s3.amazonaws.com/julia-himmel-personal-site/Projects/Cocktail+Cards/Cocktail+Cards+for+Portfolio.jpg", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/Cocktail+Cards/Cocktail+Cards+for+Portfolio2.jpg", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/Cocktail+Cards/Cocktail+Cards+for+Portfolio3.jpg", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/Cocktail+Cards/Cocktail+Cards+for+Portfolio4.jpg", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/Cocktail+Cards/Cocktail+Cards+for+Portfolio5.jpg", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/Cocktail+Cards/Cocktail+Cards+for+Portfolio6.jpg", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/Cocktail+Cards/Cocktail+Cards+for+Portfolio7.jpg", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/Cocktail+Cards/Cocktail+Cards+for+Portfolio8.jpg", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/Cocktail+Cards/Cocktail+Cards+for+Portfolio9.jpg"]
           },
           {
-            id: 5,
-            tags: ['code', 'design'],
-            title: 'Tattle App',
-            subtitle: 'Some subtitle',
-            shortDescription: 'A Short Description',
-            longDescription: 'A Long Description',
-            images: [""]
+            id: 4,
+            tags: ['design'],
+            title: 'Try The World',
+            subtitle: 'Branding and collateral for a new startup',
+            shortDescription: 'Logo, branding, packaging, and collateral for Try The World, a company that sells subscription boxes of specialty foods from around the world',
+            longDescription: 'Try The World is a company that sells subscription boxes of specialty foods from around the world. In the startup tradition of rapid prototyping, they were about to launch with an initial product but no brand, so I jumped in to create a logo, branding, packaging, and collateral for their first products. I also engaged a photographer and art-directed the product photos.',
+            images: ["https://s3.amazonaws.com/julia-himmel-personal-site/Projects/Try+The+World/TTW+Mockup+Portfolio.jpg", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/Try+The+World/TTW+Mockup+Portfolio2.jpg", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/Try+The+World/TTW+Mockup+Portfolio3.jpg", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/Try+The+World/TTW+Mockup+Portfolio4.jpg", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/Try+The+World/TTW+Mockup+Portfolio5.jpg", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/Try+The+World/TTW+Mockup+Portfolio6.jpg", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/Try+The+World/TTW+Mockup+Portfolio7.jpg", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/Try+The+World/TTW+Mockup+Portfolio8.jpg", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/Try+The+World/TTW+Mockup+Portfolio9.jpg", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/Try+The+World/TTW+Mockup+Portfolio12.jpg"]
           },
           {
-            id: 6,
+            id: 5,
             tags: ['code', 'design'],
             title: 'Tattle App',
             subtitle: 'An app to help conscious consumers make informed choices',
@@ -57,13 +57,13 @@ export default {
             images: ["https://s3.amazonaws.com/julia-himmel-personal-site/Projects/Tattle/Screen+Shot+2014-06-18+at+3.30.28+PM.png", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/Tattle/Screen+Shot+2014-06-18+at+3.30.57+PM.png", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/Tattle/Screen+Shot+2014-06-18+at+3.31.07+PM.png", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/Tattle/Screen+Shot+2014-06-18+at+3.31.23+PM.png", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/Tattle/Screen+Shot+2014-06-18+at+3.31.35+PM.png", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/Tattle/Screen+Shot+2014-06-18+at+3.31.37+PM.png"]
           },
           {
-            id: 7,
-            tags: ['code', 'design'],
-            title: 'Tattle App',
-            subtitle: 'Some subtitle',
-            shortDescription: 'A Short Description',
-            longDescription: 'A Long Description',
-            images: [""]
-          },
+            id: 6,
+            tags: ['design'],
+            title: 'Book cover for Alice Munro collection',
+            subtitle: 'Too Much Happiness',
+            shortDescription: 'This was a personal project, as Too Much Happiness is one of my favorite books. In creating the cover design, I drew each letter individually in watercolor, set the type by hand, and made the illustration for the back cover.',
+            longDescription: 'This was a personal project, as Too Much Happiness is one of my favorite books. In creating the cover design, I drew each letter individually in watercolor, set the type by hand, and made the illustration for the back cover.',
+            images: ["https://s3.amazonaws.com/julia-himmel-personal-site/Projects/Alice+Munro+book+cover/Alice+Munro+Front+for+Ipad.jpg", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/Alice+Munro+book+cover/Alice+Munro+back+for+Ipad.jpg", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/Alice+Munro+book+cover/Alice+Munro+fo+iPad+Portfolio.jpg"]
+          }
         ]
 };
--- public/bundle.js
+++ public/bundle.js
@@ -26266,19 +26266,59 @@
 	  projects: [{
 	    id: 0,
 	    tags: ['code', 'design'],
-	    title: 'Tattle App',
-	    subtitle: 'Some subtitle',
-	    shortDescription: 'A Short Description',
-	    longDescription: 'A Long Description',
-	    images: ["https://fthmb.tqn.com/INZEtkWYEpsZaksoewT_mA4DREo=/960x0/filters:no_upscale()/about/Two-kittens-GettyImages-559292093-58822e4f3df78c2ccd8b318c.jpg", "https://i.ytimg.com/vi/FFwbII94meY/maxresdefault_live.jpg"]
+	    title: 'CareerFoundry Style Guide',
+	    subtitle: 'A style guide used throughout the CareerFoundry application and marketing collateral',
+	    shortDescription: 'I created this style guide for CareerFoundry to ensure consistency throughout the brand.',
+	    longDescription: "CareerFoundry had hired a freelance designer to create a brand for the company, and she chose fonts and colors. But, without further guidance, developers and designers who worked on features for CareerFoundry would make font and color decisions on the fly, leading to a lack of consistency across the application and brand. I created a small, clear style guide when I started at the company, and it\'s still in heavy use today.",
+	    images: ["https://s3.amazonaws.com/julia-himmel-personal-site/Projects/CareerFoundry+Brandbook/Screen+Shot+2017-04-28+at+3.35.41+PM.png", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/CareerFoundry+Brandbook/Screen+Shot+2017-04-28+at+3.35.57+PM.png", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/CareerFoundry+Brandbook/Screen+Shot+2017-04-28+at+3.36.08+PM.png", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/CareerFoundry+Brandbook/Screen+Shot+2017-04-28+at+3.36.17+PM.png", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/CareerFoundry+Brandbook/Screen+Shot+2017-04-28+at+3.36.28+PM.png"]
 	  }, {
 	    id: 1,
 	    tags: ['code', 'design'],
-	    title: 'Second Project',
-	    subtitle: 'Some subtitle two',
-	    shortDescription: 'A Short Description two',
-	    longDescription: 'A Long Description two',
-	    images: ["https://fthmb.tqn.com/INZEtkWYEpsZaksoewT_mA4DREo=/960x0/filters:no_upscale()/about/Two-kittens-GettyImages-559292093-58822e4f3df78c2ccd8b318c.jpg", "https://i.ytimg.com/vi/FFwbII94meY/maxresdefault_live.jpg"]
+	    title: 'CareerFoundry Student Dashboard',
+	    subtitle: 'Complete dashboard redesign for CareerFoundry',
+	    shortDescription: 'CareerFoundry sells online, mentored courses, and the student dashboard is crucial for motivating students to continue a course, supporting the relationship with the mentor, and letting students get where they want to go within a course.',
+	    longDescription: 'CareerFoundry sells online, mentored courses, and the student dashboard is crucial for motivating students to continue a course, supporting the relationship with the mentor, and letting students get where they want to go within a course. With the guidance of a senior UX designer, I created an information architecture and sketched wireframes, and, after several rounds of feedback from the cofounders, created prototypes and conducted user testing sessions with CareerFoundry students  in our office. Then, I worked with another developer to build the dashboard.',
+	    images: ["https://s3.amazonaws.com/julia-himmel-personal-site/Projects/CareerFoundry+Dashboard/cf-dash-sketch-v1.JPG", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/CareerFoundry+Dashboard/CF-dash-june-15-v2.jpg", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/CareerFoundry+Dashboard/CF-dash-june-15-v4+(1).jpg", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/CareerFoundry+Dashboard/CF-dash-june-15-v6_June+25-3.jpg", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/CareerFoundry+Dashboard/Screen+Shot+2015-11-05+at+14.07.10.png", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/CareerFoundry+Dashboard/Screen+Shot+2015-12-09+at+17.39.52.png", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/CareerFoundry+Dashboard/Screen+Shot+2015-12-09+at+17.44.24.png", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/CareerFoundry+Dashboard/Screen+Shot+2015-12-09+at+17.44.50.png", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/CareerFoundry+Dashboard/Screen+Shot+2017-01-02+at+11.57.08.png", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/CareerFoundry+Dashboard/Screen+Shot+2017-04-28+at+1.40.48+PM.png", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/CareerFoundry+Dashboard/Screen+Shot+2017-04-28+at+1.41.02+PM.png"]
+	  }, {
+	    id: 2,
+	    tags: ['code', 'design'],
+	    title: 'CareerFoundry Course Prep',
+	    subtitle: 'An onboarding site for CareerFoundry students',
+	    shortDescription: 'With guidance from a senior UX designer, I took an onboarding site from wireframes through prototyping and user testing, then built it with one other developer.',
+	    longDescription: 'CareerFoundry sells mentored, online courses. When students would sign up for a course, it would take some time to match them with an appropriate mentor in their time zone, so the course would not begin immediately. In the interim, students had little information about the status of their course after they paid, and they would send the support staff panicked emails. In the course of doing UX research for the dashboard redesign project, I realized that some form of onboarding was needed. With guidance from a senior UX designer, I took an onboarding site from wireframes through prototyping and user testing, then built it with one other developer.',
+	    images: ["https://s3.amazonaws.com/julia-himmel-personal-site/Projects/CareerFoundry+Onboarding+Site/CF-dash-onboarding-august-13-v2-1.jpg", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/CareerFoundry+Onboarding+Site/CF-dash-onboarding-august-13-v2-2.jpg", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/CareerFoundry+Onboarding+Site/CF-dash-onboarding-august-18-v1-1.jpg", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/CareerFoundry+Onboarding+Site/CF-dash-onboarding-august-18-v1-2.jpg", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/CareerFoundry+Onboarding+Site/Screen+Shot+2017-07-20+at+5.17.00+PM.png", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/CareerFoundry+Onboarding+Site/Screen+Shot+2017-07-20+at+5.18.25+PM.png", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/CareerFoundry+Onboarding+Site/Screen+Shot+2017-07-20+at+5.18.53+PM.png", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/CareerFoundry+Onboarding+Site/Screen+Shot+2017-07-20+at+5.19.16+PM.png"]
+	  }, {
+	    id: 3,
+	    tags: ['design'],
+	    title: 'Cocktail Cards',
+	    subtitle: 'Personal project: illustrated postcards with recipes for classic cocktails',
+	    shortDescription: 'I designed these illustrated postcards as a personal project. I created the cocktail illustrations in watercolor.',
+	    longDescription: 'I designed these illustrated postcards as a personal project. I created the cocktail illustrations in watercolor.',
+	    images: ["https://s3.amazonaws.com/julia-himmel-personal-site/Projects/Cocktail+Cards/Cocktail+Cards+for+Portfolio.jpg", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/Cocktail+Cards/Cocktail+Cards+for+Portfolio2.jpg", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/Cocktail+Cards/Cocktail+Cards+for+Portfolio3.jpg", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/Cocktail+Cards/Cocktail+Cards+for+Portfolio4.jpg", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/Cocktail+Cards/Cocktail+Cards+for+Portfolio5.jpg", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/Cocktail+Cards/Cocktail+Cards+for+Portfolio6.jpg", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/Cocktail+Cards/Cocktail+Cards+for+Portfolio7.jpg", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/Cocktail+Cards/Cocktail+Cards+for+Portfolio8.jpg", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/Cocktail+Cards/Cocktail+Cards+for+Portfolio9.jpg"]
+	  }, {
+	    id: 4,
+	    tags: ['design'],
+	    title: 'Try The World',
+	    subtitle: 'Branding and collateral for a new startup',
+	    shortDescription: 'Logo, branding, packaging, and collateral for Try The World, a company that sells subscription boxes of specialty foods from around the world',
+	    longDescription: 'Try The World is a company that sells subscription boxes of specialty foods from around the world. In the startup tradition of rapid prototyping, they were about to launch with an initial product but no brand, so I jumped in to create a logo, branding, packaging, and collateral for their first products. I also engaged a photographer and art-directed the product photos.',
+	    images: ["https://s3.amazonaws.com/julia-himmel-personal-site/Projects/Try+The+World/TTW+Mockup+Portfolio.jpg", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/Try+The+World/TTW+Mockup+Portfolio2.jpg", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/Try+The+World/TTW+Mockup+Portfolio3.jpg", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/Try+The+World/TTW+Mockup+Portfolio4.jpg", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/Try+The+World/TTW+Mockup+Portfolio5.jpg", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/Try+The+World/TTW+Mockup+Portfolio6.jpg", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/Try+The+World/TTW+Mockup+Portfolio7.jpg", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/Try+The+World/TTW+Mockup+Portfolio8.jpg", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/Try+The+World/TTW+Mockup+Portfolio9.jpg", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/Try+The+World/TTW+Mockup+Portfolio12.jpg"]
+	  }, {
+	    id: 5,
+	    tags: ['code', 'design'],
+	    title: 'Tattle App',
+	    subtitle: 'An app to help conscious consumers make informed choices',
+	    shortDescription: 'Tattle let users type in the name of a product and learn whether it was owned by a larger company, whether the parent company had been the subject of any news stories that may interest a conscious consumer, and whether the product had any certifications, such as USDA Organic.',
+	    longDescription: 'Tattle let users type in the name of a product and learn whether it was owned by a larger company, whether the parent company had been the subject of any news stories that may interest a conscious consumer, and whether the product had any certifications, such as USDA Organic. We used the Freebase and New York Times Article Search APIs, and built a database of certifications by hand, since there was no centralized repository of them and they came in a variety of file formats. I built this with three team members as my final project for Dev Bootcamp. I created the design for the app, and I contributed to the code by working on the NYT API integration and writing RSpec tests.',
+	    images: ["https://s3.amazonaws.com/julia-himmel-personal-site/Projects/Tattle/Screen+Shot+2014-06-18+at+3.30.28+PM.png", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/Tattle/Screen+Shot+2014-06-18+at+3.30.57+PM.png", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/Tattle/Screen+Shot+2014-06-18+at+3.31.07+PM.png", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/Tattle/Screen+Shot+2014-06-18+at+3.31.23+PM.png", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/Tattle/Screen+Shot+2014-06-18+at+3.31.35+PM.png", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/Tattle/Screen+Shot+2014-06-18+at+3.31.37+PM.png"]
+	  }, {
+	    id: 6,
+	    tags: ['design'],
+	    title: 'Book cover for Alice Munro collection',
+	    subtitle: 'Too Much Happiness',
+	    shortDescription: 'This was a personal project, as Too Much Happiness is one of my favorite books. In creating the cover design, I drew each letter individually in watercolor, set the type by hand, and made the illustration for the back cover.',
+	    longDescription: 'This was a personal project, as Too Much Happiness is one of my favorite books. In creating the cover design, I drew each letter individually in watercolor, set the type by hand, and made the illustration for the back cover.',
+	    images: ["https://s3.amazonaws.com/julia-himmel-personal-site/Projects/Alice+Munro+book+cover/Alice+Munro+Front+for+Ipad.jpg", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/Alice+Munro+book+cover/Alice+Munro+back+for+Ipad.jpg", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/Alice+Munro+book+cover/Alice+Munro+fo+iPad+Portfolio.jpg"]
 	  }]
 	};
 
