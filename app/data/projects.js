31b5adac23d93ff0a9f6fccfdaff3766827f97dc
Julia Himmel <himmel.julia@gmail.com>
2017-07-21 16:02:36 -0400

    add cf dashboard project

--- app/data/projects.js
+++ app/data/projects.js
@@ -14,11 +14,11 @@ export default {
           {
             id: 1,
             tags: ['code', 'design'],
-            title: 'Second Project',
-            subtitle: 'Some subtitle two',
-            shortDescription: 'A Short Description two',
-            longDescription: 'A Long Description two',
-            images: [""]
+            title: 'CareerFoundry Student Dashboard',
+            subtitle: 'Complete dashboard redesign for CareerFoundry',
+            shortDescription: 'CareerFoundry sells online, mentored courses, and the student dashboard is crucial for motivating students to continue a course, supporting the relationship with the mentor, and letting students get where they want to go within a course.',
+            longDescription: 'CareerFoundry sells online, mentored courses, and the student dashboard is crucial for motivating students to continue a course, supporting the relationship with the mentor, and letting students get where they want to go within a course. With the guidance of a senior UX designer, I created an information architecture and sketched wireframes, and, after several rounds of feedback from the cofounders, created prototypes and conducted user testing sessions with CareerFoundry students  in our office. Then, I worked with another developer to build the dashboard.',
+            images: ["https://s3.amazonaws.com/julia-himmel-personal-site/Projects/CareerFoundry+Dashboard/cf-dash-sketch-v1.JPG", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/CareerFoundry+Dashboard/CF-dash-june-15-v2.jpg", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/CareerFoundry+Dashboard/CF-dash-june-15-v4+(1).jpg", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/CareerFoundry+Dashboard/CF-dash-june-15-v6_June+25-3.jpg", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/CareerFoundry+Dashboard/Screen+Shot+2015-11-05+at+14.07.10.png", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/CareerFoundry+Dashboard/Screen+Shot+2015-12-09+at+17.39.52.png", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/CareerFoundry+Dashboard/Screen+Shot+2015-12-09+at+17.44.24.png", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/CareerFoundry+Dashboard/Screen+Shot+2015-12-09+at+17.44.50.png", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/CareerFoundry+Dashboard/Screen+Shot+2017-01-02+at+11.57.08.png", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/CareerFoundry+Dashboard/Screen+Shot+2017-04-28+at+1.40.48+PM.png", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/CareerFoundry+Dashboard/Screen+Shot+2017-04-28+at+1.41.02+PM.png"]
           }
           {
             id: 3,
