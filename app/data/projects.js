d51934ef2458f926f445618efab9b4af848b7a3a
Julia Himmel <himmel.julia@gmail.com>
2017-07-21 16:07:38 -0400

    add cf course prep project

--- app/data/projects.js
+++ app/data/projects.js
@@ -23,11 +23,11 @@ export default {
           {
             id: 3,
             tags: ['code', 'design'],
-            title: 'Tattle App',
-            subtitle: 'Some subtitle',
-            shortDescription: 'A Short Description',
-            longDescription: 'A Long Description',
-            images: [""]
+            title: 'CareerFoundry Course Prep',
+            subtitle: 'An onboarding site for CareerFoundry students',
+            shortDescription: 'With guidance from a senior UX designer, I took an onboarding site from wireframes through prototyping and user testing, then built it with one other developer.',
+            longDescription: 'CareerFoundry sells mentored, online courses. When students would sign up for a course, it would take some time to match them with an appropriate mentor in their time zone, so the course would not begin immediately. In the interim, students had little information about the status of their course after they paid, and they would send the support staff panicked emails. In the course of doing UX research for the dashboard redesign project, I realized that some form of onboarding was needed. With guidance from a senior UX designer, I took an onboarding site from wireframes through prototyping and user testing, then built it with one other developer.',
+            images: ["https://s3.amazonaws.com/julia-himmel-personal-site/Projects/CareerFoundry+Onboarding+Site/CF-dash-onboarding-august-13-v2-1.jpg", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/CareerFoundry+Onboarding+Site/CF-dash-onboarding-august-13-v2-2.jpg", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/CareerFoundry+Onboarding+Site/CF-dash-onboarding-august-18-v1-1.jpg", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/CareerFoundry+Onboarding+Site/CF-dash-onboarding-august-18-v1-2.jpg", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/CareerFoundry+Onboarding+Site/Screen+Shot+2017-07-20+at+5.17.00+PM.png", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/CareerFoundry+Onboarding+Site/Screen+Shot+2017-07-20+at+5.18.25+PM.png", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/CareerFoundry+Onboarding+Site/Screen+Shot+2017-07-20+at+5.18.53+PM.png", "https://s3.amazonaws.com/julia-himmel-personal-site/Projects/CareerFoundry+Onboarding+Site/Screen+Shot+2017-07-20+at+5.19.16+PM.png"]
           },
           {
             id: 4,
