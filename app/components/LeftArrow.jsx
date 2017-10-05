5cc6e432ca186f5c63e33e016c1ed7dee9b0ca14
Julia Himmel <himmel.julia@gmail.com>
2017-10-05 19:49:28 -0400

    remove arrow elements

--- app/components/LeftArrow.jsx
+++
@@ -1,9 +0,0 @@
-var React = require('react');
-
-const RightArrow = (props) => {
-  return (
-    <div><a className='right-arrow' onClick={() => {props.onClick('left')}}>&lsaquo;</a></div>
-  );
-}
-
-module.exports = RightArrow;
